559ae7ef93122735333a8fd4478abd0997664017
Saayan Samanta <128898872+SAAYAN-SAMANTA@users.noreply.github.com>
2024-05-23 08:28:01 +0530

    Add files via upload
    Added the calulator logic

--- Calculator (Task2 codsoft)/Javascript/Cal.js
+++ Calculator (Task2 codsoft)/Javascript/Cal.js
@@ -1,3 +1,77 @@
-const buttonpressed = (e) =>{
-     console.log(e);
+let first = 0, last = 0, flag = 'f', result = 0, operator = "operator";
+
+const buttonpressed = (e) => {
+     checkorcal(e.target.innerText);
 }
+
+const checkorcal = (val) => {
+     if (val == "+" || val == "-" || val == "/" || val == "x") {
+          if (result == 0) {
+               if (operator != "operator") {
+                    let ele = document.getElementById("display");
+                    ele.innerHTML = first;
+                    }
+                    flag = 'l';
+                    operator = val;
+                    display(operator);
+               }
+               else {
+                    flag = 'l';
+                    operator = val;
+                    first = result;
+                    result = 0;
+                    last = 0;
+                    display(operator);
+               }
+          }
+
+          else if (val == "Clear") {
+               first = 0, last = 0, flag = 'f', result = 0, operator = "operator";
+               display("00");
+          }
+
+          else if (val == "=") {
+               flag = 'f';
+               switch (operator) {
+                    case "+":
+                         result = first + last;
+                         display(result);
+                         break;
+                    case "-":
+                         result = first - last;
+                         display(result);
+                         break;
+                    case "/":
+                         result = first / last;
+                         display(result);
+                         break;
+                    case "x":
+                         result = first * last;
+                         display(result);
+                         break;
+               }
+          }
+
+          else {
+               let value = parseInt(val);
+               if (flag == 'f') {
+                    first = (first * 10) + value;
+                    display(first);
+               }
+               else {
+                    last = (last * 10) + value;
+                    display(val);
+               }
+          }
+     }
+
+     const display = (message) => {
+          console.log(message);
+          let ele = document.getElementById("display");
+          if (flag == 'l') {
+               ele.innerHTML += message;
+          }
+          else {
+               ele.innerHTML = message;
+          }
+     }
