ccdfa1d2bc22fb3174a5614b8b6cf873f728395c
Saayan Samanta <128898872+SAAYAN-SAMANTA@users.noreply.github.com>
2024-05-30 15:51:09 +0530

    Landing page creation started with home page

---
+++ Landing Page (Task3 codsoft)/Javascript/Typing.js
@@ -0,0 +1,33 @@
+const txt = ["Saayan Samanta", "a FullStack Developer", "a UX/UI Developer and Designer", "a Java Developer"];
+const speed = 150;
+let characterIndex = 0;
+let currentTextIndex = 0;
+
+function removeText() {
+  const demoElement = document.getElementById("demo");
+  demoElement.textContent = "";
+}
+
+function animateTyping() {
+  const demoElement = document.getElementById("demo");
+  const currentText = txt[currentTextIndex];
+
+  if (characterIndex < currentText.length) {
+    demoElement.textContent += currentText.charAt(characterIndex);
+    characterIndex++;
+    setTimeout(animateTyping, speed);
+  } else {
+    characterIndex = 0;
+    currentTextIndex++;
+    if (currentTextIndex < txt.length) {
+      setTimeout(removeText, speed);
+      setTimeout(animateTyping, speed);
+    } else {
+      currentTextIndex = 0;
+      setTimeout(removeText, speed);
+      setTimeout(animateTyping, speed);
+    }
+  }
+}
+
+animateTyping();
